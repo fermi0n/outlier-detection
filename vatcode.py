bdc0c6aaeb68525620f0321a9f65f0e591279c0a
Dominic Mendonca <dxm@student.unimelb.edu.au@6400d-134788-l.mobility.unimelb.net.au>
2018-01-31 14:08:33 +1100

    Will shortly modify vatcode.py

---
+++ vatcode.py
@@ -0,0 +1,78 @@
+import numpy as np
+import math,random
+from scipy.spatial.distance import pdist, squareform
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+
+def VAT(R):
+    """
+
+    VAT algorithm adapted from matlab version:
+    http://www.ece.mtu.edu/~thavens/code/VAT.m
+
+    Args:
+        R (n*n double): Dissimilarity data input
+        R (n*D double): vector input (R is converted to sq. Euclidean distance)
+    Returns:
+        RV (n*n double): VAT-reordered dissimilarity data
+        C (n int): Connection indexes of MST in [0,n)
+        I (n int): Reordered indexes of R, the input data in [0,n)
+    """
+
+    R = np.array(R)
+    N, M = R.shape
+    if N != M:
+        R = squareform(pdist(R))
+
+    J = list(range(0, N))
+
+    y = np.max(R, axis=0)
+    i = np.argmax(R, axis=0)
+    j = np.argmax(y)
+    y = np.max(y)
+
+    I = i[j]
+    del J[I]
+
+    y = np.min(R[I,J], axis=0)
+    j = np.argmin(R[I,J], axis=0)
+
+    I = [I, J[j]]
+    J = [e for e in J if e != J[j]]
+
+    C = [1,1]
+    for r in range(2, N-1):
+        y = np.min(R[I,:][:,J], axis=0)
+        i = np.argmin(R[I,:][:,J], axis=0)
+        j = np.argmin(y)
+        y = np.min(y)
+        I.extend([J[j]])
+        J = [e for e in J if e != J[j]]
+        C.extend([i[j]])
+
+    y = np.min(R[I,:][:,J], axis=0)
+    i = np.argmin(R[I,:][:,J], axis=0)
+
+    I.extend(J)
+    C.extend(i)
+
+    RI = list(range(N))
+    for idx, val in enumerate(I):
+        RI[val] = idx
+
+    RV = R[I,:][:,I]
+
+    return RV.tolist(), C, I
+
+
+if __name__ == "__main__":
+
+    vote= pd.read_csv('vote1.csv',dtype=None)
+    toyl=vote[['f1','f2','f3','f4','f5','f6','f7','f8','f9','f10','f11','f12','f13','f14','f15','f16']].as_matrix().tolist()
+    sq = squareform(pdist(toyl))
+    RV, C, I = VAT(sq)
+    ax=sns.heatmap(RV,cmap='Greys_r',xticklabels=False,yticklabels=False)
+    ax.set(xlabel='Objects', ylabel='Objects')
+    plt.show(ax)
